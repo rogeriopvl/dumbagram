3362a2a333733b5a98dc499849d75caa6c615955
rogeriopvl <rogeriopvl@gmail.com>
2015-03-16 22:53:31 +0000

    Add localStorage support for history

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,14 @@
         'of', 'the', 'a'
     ];
 
+    var historyCache;
+
+    if (window.localStorage.dumbagram) {
+        historyCache = JSON.parse(window.localStorage.dumbagram);
+    } else {
+        historyCache = [];
+    }
+
     var swapInitials = function (s) {
         s = s.split(/\s+/g);
 
@@ -16,40 +24,52 @@
         var tmp = '';
 
         for (var i = 0; i < division; i++) {
-            tmp = s[i][0]
+            tmp = s[i][0];
 
-            if (STOP_WORDS.indexOf(s[i].toLowerCase()) !== -1
-                || STOP_WORDS.indexOf(s[last - i].toLowerCase()) !== -1) continue;
+            if (STOP_WORDS.indexOf(s[i].toLowerCase()) !== -1 ||
+                STOP_WORDS.indexOf(s[last - i].toLowerCase()) !== -1) continue;
 
-            s[i] = s[last - i][0] + s[i].substring(1)
-            s[last - i] = tmp + s[last - i].substring(1)
+            s[i] = s[last - i][0] + s[i].substring(1);
+            s[last - i] = tmp + s[last - i].substring(1);
         }
 
         return s.join(' ');
-    }
+    };
+
+    var addToHistory = function (items) {
+        var tr = document.createElement('tr');
+        tr.innerHTML = [
+            '<td class="align-center">' + items[0] + '</td>',
+            '<td class="align-center">' + items[1] + '</td>'
+        ].join('');
+        table.appendChild(tr);
+    };
 
     var formSubmit = function (ev) {
-        if (document.querySelectorAll('td').length < 1) {
-            document.querySelector('#history').classList.remove('hide-all');
-        }
-
         if (ev) { ev.preventDefault(); }
 
         var res = swapInitials(input.value);
         document.querySelector('#result h1').textContent = res;
-        var tr = document.createElement('tr');
-        tr.innerHTML = [
-            '<td class="align-center">' + input.value + '</td>',
-            '<td class="align-center">' + res + '</td>'
-        ].join('');
-        table.appendChild(tr);
+        addToHistory([input.value, res]);
+        historyCache.push([input.value, res]);
 
+        if (historyCache.length > 0) {
+            document.querySelector('#history').classList.remove('hide-all');
+        }
+        window.localStorage.dumbagram = JSON.stringify(historyCache);
     };
 
     var hashCallback = function () {
         input.value = window.location.hash.substr(1);
         formSubmit();
     };
+
+    if (historyCache.length > 0) {
+        document.querySelector('#history').classList.remove('hide-all');
+    }
+    historyCache.forEach(function (item) {
+        addToHistory(item);
+    });
 
     if (window.location.hash) { hashCallback(); }
     window.addEventListener('hashchange', hashCallback);
